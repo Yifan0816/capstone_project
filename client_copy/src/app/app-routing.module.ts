9a7b8d56e0166972c1ddacb673eaa2ea4b8dbec8
Yifan0816 <z.yifan0816@gmail.com>
2022-09-21 17:20:23 -0500

    Get rid of submodule

---
+++ client_copy/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { GroupsComponent } from './main/groups/groups.component';
+import { HomeComponent } from './main/home/home.component';
+import { OrganizationsComponent } from './main/organizations/organizations.component';
+
+const fallbackRoute: Route = { path: '**', component: HomeComponent };
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'organizations', component:OrganizationsComponent},
+  {path:'groups', component:GroupsComponent},
+  fallbackRoute
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
