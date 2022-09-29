791926b15db59eed8e4aa8fb5501ac670aa1a5b8
Yifan0816 <z.yifan0816@gmail.com>
2022-09-29 10:52:11 -0500

    Attempts to fix search bar

--- client/src/app/header/search/search.component.ts
+++ client/src/app/header/search/search.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GroupsService } from 'src/app/service/groups.service';
 import { Animaltype } from 'src/models/animaltypes';
 import { Shelter } from 'src/models/shelter';
+import {
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel,
+  RoutesRecognized,
+} from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -14,31 +21,64 @@ export class SearchComponent implements OnInit {
   searchResult!: Array<Animaltype>;
   animalTypes!: Animaltype[];
   selectedType!: any;
+  isLeaving = false;
+  names!: any[];
 
   constructor(
     private groupsService: GroupsService,
     private router: Router,
-  ) {}
-
-  routeTo(animalType: Animaltype) {
-    this.router.navigate([`/shelters/${animalType.ShelterId}/animaltypes/${animalType.AnimalTypeId}`]);
-  }
-  getAllAnimalTypes(): void {
-    this.groupsService.getAllAnimalTypes().subscribe({
-      next: (res) => (this.animalTypes = res),
+    private route: ActivatedRoute
+  ) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        console.log(event);
+        this.isLeaving = true;
+        this.searchResult = [];
+      }
+      if (event instanceof NavigationEnd) {
+        console.log(event);
+        this.isLeaving = false;
+        console.log(this.route.snapshot);
+        //
+      }
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
     });
   }
 
-  filterFilteredAnimalTypes(event:any):any {
+  routeTo(animalType: Animaltype) {
+    this.router.navigate(
+      [
+        `/shelters/${animalType.ShelterId}/animaltypes/${animalType.AnimalTypeId}`,
+      ],
+      {
+        relativeTo: this.route,
+      }
+    );
+    // window.location.reload();
+  }
+
+  getAllAnimalTypes(): void {
+    this.groupsService.getAllAnimalTypes().subscribe({
+      next: (res) => (this.animalTypes = res)
+    });
+  }
+
+  filterFilteredAnimalTypes(event: any): any {
     let filtered: any[] = [];
-    this.animalTypes.forEach(type=> {
-      if(type.AnimalTypeName.toLowerCase().includes(event.target.value.toLowerCase())) {
+    this.animalTypes.forEach((type) => {
+      if (
+        type.AnimalTypeName.toLowerCase().includes(
+          event.target.value.toLowerCase()
+        )
+      ) {
         filtered.push(type);
       }
     });
     this.searchResult = [...filtered];
   }
-
 
   ngOnInit(): void {
     this.getAllAnimalTypes();
